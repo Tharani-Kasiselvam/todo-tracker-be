e86e1697ac8b35e1242a8e46f2daef082d245ba4
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2025-04-20 19:49:49 +0530

    enabled Delete Api and List Todo API

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -3,7 +3,9 @@ const todoController = require("../controllers/todoController.js")
 
 const router = express.Router()
 
+router.get("/all-todos",todoController.getTodoData)
 router.post("/create-todo", todoController.createTodo)
 router.put("/update-todo/:id", todoController.updateTodo)
+router.delete("/delete-todo/:id", todoController.deleteTodo)
 
 module.exports = router
